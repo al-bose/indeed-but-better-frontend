26bf73b299c1981e74c36551c4bc44836c33d929
patrickmwilson <patrickmwilson@ucla.edu>
2023-06-03 22:46:50 -0700

    Added profile page first components

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {User} from "../services/user/user";
+import {Router} from "@angular/router";
+import {UserService} from "../services/user/user.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+
+  user: User = JSON.parse(localStorage.getItem('currentUser')!);
+
+  constructor(private router: Router, private userService: UserService) {}
 
 }
