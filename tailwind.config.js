519d5e4672c2dae4e254727ec72ac1c933cb812c
patrickmwilson <patrickmwilson@ucla.edu>
2023-06-09 01:23:20 -0700

    Modify tailwind config to extend base theme, not override

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    colors: {
-      'ibb-black': '#1a1e26',
-      'ibb-dark-black': '#121212',
-      'ibb-gradient-mid': '#1e2128',
-      'ibb-light-black': '#1f242d',
-      'ibb-white': '#c6c7c8',
-      'ibb-dark-green': '#4db9a2',
-      'ibb-border-grey': '#2f3f4d',
-      'ibb-button-grey': '#2f3f4d',
-      'ibb-button-green-hover': '#57d7ba',
-      'pure-white': '#FFFFFF'
-    },
-    extend: {},
+    extend: {
+      colors: {
+        'ibb-black': '#1a1e26',
+        'ibb-dark-black': '#121212',
+        'ibb-gradient-mid': '#1e2128',
+        'ibb-light-black': '#1f242d',
+        //'ibb-white': '#c6c7c8',
+        'ibb-white': '#FFFFFF',
+        'ibb-grey': '#c6c7c8',
+        'ibb-dark-green': '#4db9a2',
+        'ibb-darker-green': '#4ade80',
+        'ibb-border-grey': '#2f3f4d',
+        'ibb-button-grey': '#2f3f4d',
+        'ibb-button-green-hover': '#57d7ba',
+        'pure-white': '#FFFFFF'
+      }},
   },
   plugins: [
     require('@tailwindcss/forms')
